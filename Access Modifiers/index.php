c5d38396da3246d64b95a1cffe6ff066269476dc
Hamid Achaou <hamidAchaou379@gmail.com>
2023-10-10 02:03:10 -0700

    add

--- Access Modifiers/index.php
+++ Access Modifiers/index.php
@@ -1,35 +1,8 @@
 <?php
-class Stagire {
-    public $Nom; // Public variable
-
-    protected $Prenom; //Protected variable 
-
-    private $Age; // Private variable
-
-    public function __construct($Nom) {
-        $this->Nom = $Nom; // Public constructor
-    }
-
-    // Private method
-    private function gteNom() {
-        echo "Achaou";
-    }
-
-    // Public method
-    public function getAge() {
-        echo $this->Age;
-    }
-
-    // Protected method
-    protected function setPrenom() {
-        $this->Prenom = "Hamid";
-    }
-
-    // Methode destruct
-    public function __destruct()
-    {
-        echo "Welcom in Destruct";
-    }
-
- }
+require_once "./stagire.php";
+$stagire = new Stagire("Hamid");
+// $getNom = $stagire->getNom();
+// echo $getNom;
+$getAge = $stagire->getAge();
+echo $getAge;
 ?>
